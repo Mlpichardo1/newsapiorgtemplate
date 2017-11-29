8cdc1357e6a35c3f3f7cdea0e19648aa7b721f76
Manuel Pichardo <mlpichardo1@gmail.com>
2017-11-29 00:15:04 +0000

    Dynamic Dropdown

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-
 /*global $
-ftoi
 */
 $(document).ready(function(){
-    var feet = ftoi * 24;
+    $.ajax({
+  method: "GET",
+  url: "https://newsapi.org/v2/sources",
+  data: { category:"business", country:"us", language:"en", apikey:"c2e0b8fb832b4285ab83496cc839d217" },
+  success: function(data) {
+      if (data.status === "ok") {
+          for (var i = 0; i < data.sources.length; i++){
+             var source = document.createElement("OPTION");
+             source.innerHTML = data.sources[i].name;
+             document.getElementById('selection').appendChild(source);
+          }
+      
+      }
+  }
+        
+    })
+//   .done(function( msg ) {
+//     console.log( "Data Saved: " + msg );
+//   });
 }) 
 
 
-$.ajax({
-  method: "POST",
-  url: "some.php",
-  data: { name: "John", location: "Boston" }
-})
-  .done(function( msg ) {
-    alert( "Data Saved: " + msg );
-  });
