8ef0a3d62d8846c0309f19586f8efde31c794f17
Manuel Pichardo <mlpichardo1@gmail.com>
2017-12-01 02:27:33 +0000

    Working API

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
-/*global $
-*/
-$(document).ready(function(){
-    $.ajax({
-  method: "GET",
-  url: "https://newsapi.org/v2/sources",
-  data: { category:"business", country:"us", language:"en", apikey: APIKEY},
-  success: function(data) {
-      if (data.status === "ok") {
-          for (var i = 0; i < data.sources.length; i++){
-             var source = document.createElement("OPTION");
-             source.setAttribute("value", data.sources[i].id);
-             source.innerHTML = data.sources[i].name;
-             document.getElementById('selection').appendChild(source);
-          }
-      }
-  }
-    })
-//   .done(function( msg ) {
-//     console.log( "Data Saved: " + msg );
-//   });
-
-    $("#source").submit(function(event) {
-        event.preventDefault();
-        alert(document.getElementById("selection").value)
-        $.ajax({
-            method: "GET",
-            url: "https://newsapi.org/v2/top-headlines?sources=bbc-news",
-            data: { sources: document.getElementById('selection').value, country:"us", language:"en", apikey: APIKEY},
-            success: function(show) {
-                
-            }
-        })
-    });  
-}) 
-
-
+/*global $ APIKEY
+ */
+$(document).ready(function() {
+	$.ajax({
+		method: "GET",
+		url: "https://newsapi.org/v2/sources",
+		data: {
+			category: "business",
+			country: "us",
+			language: "en",
+			apikey: APIKEY
+		},
+		success: function(data) {
+			if (data.status === "ok") {
+				for (var i = 0; i < data.sources.length; i++) {
+					var source = document.createElement("OPTION");
+					source.setAttribute("value", data.sources[i].id);
+					source.innerHTML = data.sources[i].name;
+					document.getElementById('selection').appendChild(source);
+				}
+			}
+		}
+	})
+});
+$("#source").submit(function(event) {
+	event.preventDefault();
+	$.ajax({
+		method: "GET",
+		url: "https://newsapi.org/v2/top-headlines",
+		data: {
+			sources: document.getElementById('selection').value,
+			country: "us",
+			language: "en",
+			category: "business",
+			apikey: APIKEY
+		},
+		success: function(show) {
+			if (show.status === "ok") {
+				for (var i = 0; i < show.articles.length; i++) {
+					var choice = document.createElement("LI");
+					choice.innerHTML = "<img src=" + show.articles[i].urlToImage + " height= 100vh> <br> " + show.articles[i].title + " <br> " + show.articles[i].description + "";
+					document.getElementById('newsart').appendChild(choice);
+				}
+			}
+		}
+	})
+});
